2ddc2772de58433a29e3fc98990dfac7a9ea7a80
hamza <hamza@hamza.com>
2022-07-18 16:28:15 +0200

    test branch

--- src/pages/404.js
+++ src/pages/404.js
@@ -26,7 +26,7 @@ const NotFound = () => (
           }}
         >
           <Typography align="center" color="textPrimary" variant="h1">
-            404: The page you are looking for is not here
+            404:: The page you are looking for is not here
           </Typography>
           <Typography align="center" color="textPrimary" variant="subtitle2">
             You either tried some shady route or you came here by mistake. Whichever it is, try
